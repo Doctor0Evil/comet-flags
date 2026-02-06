cf78d3335f83bac6581280c79591637e5af19fb8
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2026-02-06 16:18:54 -0700

    Create Warning.tsx

---
+++ comet-flags-webpanel/src/components/Warning.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Warning: React.FC = () => (
+  <div className="warning">
+    <strong>WARNING: EXPERIMENTAL FEATURES AHEAD!</strong> By enabling these
+    features, you could lose browser data or compromise your security or
+    privacy under conventional frameworks. Enabled features apply to all users
+    of this browser.
+  </div>
+);
+
+export default Warning;
