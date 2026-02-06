60a74cbc557f652a21b91e209356ee24c0e3080e
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2026-02-06 16:18:37 -0700

    Create Header.tsx

---
+++ comet-flags-webpanel/src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header: React.FC = () => (
+  <div className="header">
+    <h1>Comet Flags</h1>
+    <p>Access experimental features and Legal.Banannas protocols.</p>
+  </div>
+);
+
+export default Header;
